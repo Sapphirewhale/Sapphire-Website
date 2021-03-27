2c8a9efc33864927121cfb12734dd69e807c3883
Sami Yildirim <professoryildirim@yahoo.com.au>
2021-03-27 21:59:24 +1100

    App changes

--- sapphire-website/src/App.js
+++ sapphire-website/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import WelcomePage from './components/welcome-page.component';
+
+import "./App.css";
+import WelcomePage from "./components/welcome-page.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <WelcomePage/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <nav className="App-header">
+          <div className="navbar-brand">
+            Sapphire's Site!
+          </div>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+                Home
+            </li>
+          </div>
+        </nav>
+        <div>
+        <WelcomePage/>
+        </div>
+      </div>
   );
 }
 
