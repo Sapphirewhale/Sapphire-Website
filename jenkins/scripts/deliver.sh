8b703ce4d6658a5e519b9ccea8047077d8d8afa5
Sami Yildirim <professoryildirim@yahoo.com.au>
2021-03-28 19:09:17 +1100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,9 +3,11 @@
 echo 'Changing directories'
 cd sapphire-website
 
-echo 'Building the app'
+echo 'Installing the app'
 npm install --unsafe-perm=true --allow-root
-npm run build
+
+echo 'Building the app'
+npm run build --unsafe-perm=true --allow-root
 
 echo 'Serving the app on port 4000'
 #serve -s build -l 4000
