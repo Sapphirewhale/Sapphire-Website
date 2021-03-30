c743d8e73b0ba5fbda4f46ffdb6fc7bf6c9c7f13
Sami Yildirim <professoryildirim@yahoo.com.au>
2021-03-30 20:30:13 +1100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -10,5 +10,6 @@ echo 'Building the app'
 npm run build --unsafe-perm=true --allow-root
 
 echo 'Serving the app on port 4000'
+chmod +x -R /usr/local/lib/node_modules
 npm install -g serve
 serve -s build -l 4000
